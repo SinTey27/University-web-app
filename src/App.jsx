b5d817785bf0f9e08f9126be864f9a642fdb4fc1
Sintey <sintay2729@gmail.com>
2025-08-01 15:59:44 +0300

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,23 @@ import Hero from "./component/Hero/Hero";
 import Programs from "./component/Programs/Programs";
 import Title from "./component/Title/Title";
 import About from "./component/About/About";
+import Campus from "./component/Campus/Campus";
+import Testimonials from "./component/Testimonials/Testimonials";
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Hero />
+
       <div className="container">
         <Title subTitle="Our Programs" title="What We Offer" />
         <Programs />
         <About />
+        <Title subTitle="Gallery " title="Campus Photos" />
+        <Campus />
+        <Title subTitle="TESTIMONIALS " title="What Student Says" />
+        <Testimonials />
       </div>
     </div>
   );
