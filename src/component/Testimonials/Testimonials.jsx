d77d3f8436d34026531f2d31b5bdac6731ea11a1
Sintey <sintay2729@gmail.com>
2025-08-04 14:32:31 +0300

    last updated

--- src/component/Testimonials/Testimonials.jsx
+++ src/component/Testimonials/Testimonials.jsx
@@ -12,6 +12,74 @@ function Testimonials() {
     <div className="testimonials">
       <img src={next_icon} alt="" className="next-btn" />
       <img src={back_icon} alt="" className="back-btn" />
+      <div className="slider">
+        <ul>
+          <li>
+            <div className="slide">
+              <div className="user-info">
+                <img src={user_1} alt="" />
+                <div>
+                  <h3>Tigist Getabalew</h3>
+                  <span>Mizan-Tepi, Ethiopia</span>
+                </div>
+              </div>
+              <p>
+                "The diverse community and cutting-edge resources at Mizan-Tepi
+                University have transformed my learning experience. I feel
+                prepared for the real world!"
+              </p>
+            </div>
+          </li>
+          <li>
+            <div className="slide">
+              <div className="user-info">
+                <img src={user_2} alt="" />
+                <div>
+                  <h3>Sintayehu Getabalew</h3>
+                  <span>Addis Ababa, Ethiopia</span>
+                </div>
+              </div>
+              <p>
+                "The hands-on projects and supportive faculty at Addis Ababa
+                University have been instrumental in my growth as an engineer. I
+                couldn't ask for a better environment!"
+              </p>
+            </div>
+          </li>
+          <li>
+            <div className="slide">
+              <div className="user-info">
+                <img src={user_3} alt="" />
+                <div>
+                  <h3>Maya H/mikael</h3>
+                  <span>Arbaminich, Ethiopia</span>
+                </div>
+              </div>
+              <p>
+                Studying at Arbaminch University has allowed me to explore my
+                creativity while receiving top-notch guidance. It’s truly a
+                nurturing space for artists."
+              </p>
+            </div>
+          </li>
+          <li>
+            <div className="slide">
+              <div className="user-info">
+                <img src={user_4} alt="" />
+                <div>
+                  <h3>Yabest H/mikael</h3>
+                  <span>Jimma, Ethiopia</span>
+                </div>
+              </div>
+              <p>
+                "The innovative curriculum and collaborative atmosphere at Jimma
+                University have sharpened my skills and opened countless
+                opportunities in tech."
+              </p>
+            </div>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
